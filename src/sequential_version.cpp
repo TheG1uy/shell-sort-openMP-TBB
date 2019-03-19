b0df351c4a5417b6b39802f3d39402e52ac615d6
TheG1uy <paulnvidia@gmail.com>
2019-03-19 10:41:19 +0300

    changed time detection

--- src/sequential_version.cpp
+++ src/sequential_version.cpp
@@ -5,16 +5,17 @@
 #include <iostream>
 #include <omp.h>
 
+
 template <typename T>
-void insert_sort(std::vector<T> &arr, int i, int half) {
+void insert_sort(std::vector<T> &arr, int i, int step) {
 	int j = 0;
-	for (int k = half + i; k < arr.size(); k = k + half)
+	for (int k = step + i; k < arr.size(); k = k + step)
 	{
 		j = k;
-		while (j > i && arr[j - half] > arr[j])
+		while (j > i && arr[j - step] > arr[j])
 		{
-			std::swap(arr[j], arr[j - half]);
-			j = j - half;
+			std::swap(arr[j], arr[j - step]);
+			j = j - step;
 		}
 	}
 }
@@ -25,7 +26,7 @@ std::vector<T> shell_sort_parallel(std::vector<T> arr)
 	assert(arr.size() > 0);
 	auto end = arr.size() - 1;
 
-	unsigned h = end / 2;
+	unsigned h;
 	for (h = 1; h <= end / 9; h = 3 * h + 1)
 		;
 	int i;
@@ -35,7 +36,7 @@ std::vector<T> shell_sort_parallel(std::vector<T> arr)
         #pragma omp parallel for shared(arr, size, h) private (i) default(none)
 		for (i = 0; i < h; i++)
 		{
-			InsertSort(arr, i, h);
+			insert_sort(arr, i, h);
 		}
 	}
 	return arr;
@@ -47,7 +48,7 @@ std::vector<T> shell_sort(std::vector<T> arr)
 	assert(arr.size() > 0);
 	auto end = arr.size() - 1;
 
-	unsigned h = end / 2;
+	unsigned h;
 	for (h = 1; h <= end / 9; h = 3 * h + 1)
 		;
 	for (; h > 0; h /= 3)
@@ -66,7 +67,7 @@ std::vector<T> shell_sort(std::vector<T> arr)
 }
 
 int main() {
-	constexpr auto test_size = 100000;
+	constexpr auto test_size = 10000000;
 	std::mt19937 gen(time(0));
 	std::uniform_int_distribution<> random(0, 1000000);
 	std::vector<int> unsorted_array;
@@ -76,21 +77,20 @@ int main() {
 		unsorted_array.emplace_back(random(gen));
 
 
-
 	std::cout << std::endl;
-	float fTimeStart = clock() / (float)CLOCKS_PER_SEC;
+	float fTimeStart = omp_get_wtime();
 	auto sorted_array = shell_sort(unsorted_array);
-	float fTimeStop = clock() / (float)CLOCKS_PER_SEC;
+	float fTimeStop = omp_get_wtime();
 
 	std::cout << "sequential_version time : " << fTimeStop - fTimeStart << std::endl;
 
 	std::cout << std::endl;
-	fTimeStart = clock() / (float)CLOCKS_PER_SEC;
+	fTimeStart = omp_get_wtime();
 	auto parallel_sorted_array = shell_sort_parallel(unsorted_array);
-	fTimeStop = clock() / (float)CLOCKS_PER_SEC;
+	fTimeStop = omp_get_wtime();
 	std::cout << "parallel_version time : " << fTimeStop - fTimeStart << std::endl;
-	/*
-	for (auto&& elem : sorted_array)
+	
+	/*for (auto&& elem : sorted_array)
 		std::cout << elem << " ";
 	std::cout << std::endl;
 	for (auto&& elem : parallel_sorted_array)
